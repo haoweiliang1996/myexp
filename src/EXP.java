e294df659adfdf2456fe1f538924d699551b2afe
Scottliang <liang199611@outlook.com>
2016-11-27 17:02:04 +0800

    完成模式删除，准备格式化输出

--- src/EXP.java
+++ src/EXP.java
@@ -8,16 +8,19 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.security.Key;
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.FileWriter;
+import java.util.stream.*;
+import java.util.function.*;
 
 public class EXP {
     public static HashMap<String,String> patternMap = new HashMap<>();
     public static HashMap<String,Integer> count2=new HashMap<String,Integer>();
     public static HashMap<String,Integer> count3=new HashMap<String,Integer>();
-    public static HashMap<String,String[]> pattern_to_class=new HashMap<>();
     public static HashMap<String,Vector<String>> pattern_to_deny_pattern=new HashMap<>();
     public static String NoPattern="";
     public static myTreeKount treeCount;
@@ -32,6 +35,7 @@ public class EXP {
         private int pre[];
         private HashMap<String,Integer> key_to_keyid=new HashMap<>();
         private Vector<String> keyList=new Vector<>();
+
         private int countHeadTab(String s){
             int kount=0;
             for(char c:s.toCharArray()){
@@ -50,8 +54,6 @@ public class EXP {
             }
 
             pre[keyId]=keyIdOfFather;
-            //if(keyId==keyList.size()-1||(countHeadTab(keyList.get(keyId))>=countHeadTab(keyList.get(keyId+1)))) //如果处理到了keyList结尾或者tab不比下一条语句少的就结束
-              //  return;
             for(int i=keyId+1;i<keyList.size();i++) {
                 if(countHeadTab(keyList.get(keyId))>=countHeadTab(keyList.get(i)))
                     return;
@@ -59,6 +61,7 @@ public class EXP {
                     buildTree(i, keyId);
             }
         }
+
         public myTreeKount(String strFile) throws Exception
         {
             File file=new File(strFile);
@@ -70,10 +73,16 @@ public class EXP {
                 while ((line = br.readLine()) != null) {
                     int splitIndex=line.lastIndexOf('\t');
                     if(splitIndex==-1) {
-                        System.out.println("info "+"这行的class无效");
-                        continue;
+                        if(!line.equals(NoPattern)){
+                            System.out.println("info "+"这行的class无效");
+                            continue;
+                        }
                     }
-                    String key=line.substring(0,splitIndex);
+                    String key;
+                    if(splitIndex==-1)
+                        key=line;
+                    else
+                        key=line.substring(0,splitIndex);
                     key_to_keyid.put(key.trim(),keyList.size());
                     keyList.add(key);
                 }
@@ -97,16 +106,17 @@ public class EXP {
         }
         public int getKeyId(String key){
             System.out.println("debug getKeyId:"+key);
+            if(!key_to_keyid.containsKey(key))
+                return FLAG.ROOT;
             return key_to_keyid.get(key);
         }
         public String getKeyById(int id){
-           // System.out.println("debug getKeyById id:"+id);
+            // System.out.println("debug getKeyById id:"+id);
             if(id>=keyList.size())
                 System.out.println("KeyList下标越界："+id+" "+keyList.size());
             return keyList.get(id);
         }
     }
-
 
     public static void loadPattern(String strFile)
             throws Exception {
@@ -138,35 +148,58 @@ public class EXP {
                     //添加以处理 W1|W2*W3|W4的输入
                     String[] vauleArray=value.split("\\s+");
                     //System.out.println("debug"+vauleArray.length);
-                    StringBuffer sb=new StringBuffer();
+                    StringBuilder sb=new StringBuilder();
                     for (int i=0;i<vauleArray.length;i++){
                         //System.out.print("debug"+vauleArray[i]);
+
                         //《处理要去除的模式的输入
-                        int throwBeginIndex=vauleArray[i].indexOf('-');
+                        int throwBeginIndex=vauleArray[i].indexOf("-");
+
+                        Vector<String> temp=new Vector<>();
                         if(throwBeginIndex!=-1){
-                            String[] throwStrList=vauleArray[i].substring(throwBeginIndex).split(",");//to-do
+                            String[] throwStrList=vauleArray[i].substring(throwBeginIndex+1).split(",");//to-do
                             vauleArray[i]=vauleArray[i].substring(0,throwBeginIndex);
+                            for(String str:throwStrList){
+                                if(!str.contains("|"))
+                                    temp.add(str);
+                                else
+                                {
+                                    StringBuilder sbuilder=new StringBuilder();
+                                    String[] strTempList=str.split("\\.\\*");
+                                    for(String strTemp:strTempList){
+                                        if(strTemp.contains("|"))
+                                            sbuilder.append("("+strTemp+")"+".*");
+                                        else
+                                            sbuilder.append(strTemp+".*");
+                                    }
+                                    if(sbuilder.length()>0)
+                                        temp.add(sbuilder.delete(sbuilder.length()-2,sbuilder.length()).toString());
+                                }
+                            }
                         }
                         //处理要去除的模式的输入》
-                        String[] phaseArrary=vauleArray[i].split("\\.\\*");
-                        //System.out.println("debug"+phaseArrary.length);
-                        String nowPrasePattern="";
-                        for (int j=0;j<phaseArrary.length;j++) {
-                            String s = phaseArrary[j];
-                            if (s.contains("|"))
-                                s = "(" + s + ")";
-                            if (j != phaseArrary.length - 1)
-                                s += ".*";
-                            //System.out.println("debug"+s);
-                            sb.append(s);
-                            if (j==phaseArrary.length-1)
-                                nowPrasePattern=sb.toString();
+
+                        if(vauleArray[i].contains("|")) {
+                            String[] phaseArrary = vauleArray[i].split("\\.\\*");
+                            //System.out.println("debug"+phaseArrary.length);
+                            for (String s:phaseArrary) {
+                                if (s.contains("|"))
+                                    s = "(" + s + ")";
+                                //System.out.println("debug"+s);
+                                sb.append(s+".*");
+                            }
+                            sb.delete(sb.length()-2,sb.length());
                         }
-                        System.out.println("debug "+nowPrasePattern);
-                        pattern_to_class.put(nowPrasePattern,key.split("\\|"));
+                        else
+                            sb.append(vauleArray[i]);
+
+                        String nowPrasePattern=sb.substring(sb.lastIndexOf("@")+1);
+                        System.out.println("debug nowPrasePattern:"+nowPrasePattern);
+                        if(!pattern_to_deny_pattern.containsKey(nowPrasePattern))
+                            pattern_to_deny_pattern.put(nowPrasePattern,temp);
+
                         if(i!=vauleArray.length-1)
-                            //不同的模式间的间隔符是@
-                            sb.append("@");
+                            sb.append("@");                     //不同的模式间的间隔符是@
                     }
                     if (sb.length()>0) {
                         value = sb.toString();
@@ -181,7 +214,6 @@ public class EXP {
             }
             br.close();
             System.out.println(patternMap);
-            System.out.println("debug"+"pattern_to_class "+pattern_to_class.toString());
             System.out.println("debug "+"pattern_to_deny_pattern "+pattern_to_deny_pattern.toString());
         }
         else {
@@ -231,6 +263,7 @@ public class EXP {
                         System.out.println("drop" + line);
                         continue;
                     }
+
                     String resultTemp=line;
                     for(int i=2;i<4;i++) {
                         String line_cs=line_c[i];
@@ -243,11 +276,18 @@ public class EXP {
                         line_cs = line_cs.replaceAll("，，", "，");
                         line_cs = line_cs.replaceAll("，，，", "，");
                         String result = type(line_cs).trim();
+
                         //统计行业
+                        if(result.length()==0) {
+                            System.out.println("debug + line_cs: " + line_cs + " result:" + result);
+                            System.out.println("因为某种情况 删除了所有找到的class");
+                            result="待处理";
+                        }
                         String[] result_split=result.split("\\|");
                         HashSet<String> keyIdSet=new HashSet<>();
                         for(String str:result_split){
-                            for(int id=treeCount.getKeyId(str);treeCount.getFatherKeyId(id)!=-2;id=treeCount.getFatherKeyId(id)){
+                            final int FLAG_ROOT=-2;
+                            for(int id=treeCount.getKeyId(str);id!=FLAG_ROOT;id=treeCount.getFatherKeyId(id)){
                                 keyIdSet.add(treeCount.getKeyById(id));
                             }
                         }
@@ -265,6 +305,7 @@ public class EXP {
                             }
                         }
                         //统计行业
+
                         resultTemp += "\t" + result;
                     }
                     rList.add(resultTemp);
@@ -293,71 +334,112 @@ public class EXP {
 
         while(iter.hasNext()){
             Entry<String, String> entry = iter.next();
-            //String key = entry.getKey();
+            String key = entry.getKey();
             String regex = entry.getValue();
 
             String matchedPattern=isKeyType(regex,line);
-            if (matchedPattern.length()!=0){
-                if(sb.indexOf(matchedPattern) == -1)
-                    sb.append(matchedPattern + "@");
-
-            }
+            if(matchedPattern.length()==0)
+                continue;
+            matchedPattern+="\t"+key;
+            System.out.println("debug "+"matchedPattern "+matchedPattern);
+            sb.append(matchedPattern + "@");
         }
         String type= NoPattern;
         if (sb.length() != 0) {
-            String[] patternList = sb.substring(0, sb.length() - 1).split("@");//去除末尾的‘|’
+            System.out.println("debug typeResult"+sb.substring(0,sb.length()-1));
+            //<<处理去除模式
+            String[] typeResult = sb.substring(0, sb.length() - 1).split("@");//去除末尾的‘@’
+            String[] phaseList=new String[typeResult.length];
+            String[] classList=new String[typeResult.length];
+            String[] patternList=new String[typeResult.length];
+            for(int i=0;i<typeResult.length;i++){
+                String[] temp=typeResult[i].split("\\t");
+                phaseList[i]=temp[0];
+                patternList[i]=temp[1];
+                classList[i]=temp[2];
+            }
             HashSet<String> denyPatternSet=new HashSet<String>();
             for(String pa:patternList){
                 if(!pattern_to_deny_pattern.containsKey(pa))
+                {
+                    System.out.println("error+ "+pa+"没有出现在 pattern_to_deny_pattern");
                     continue;
+                }
                 for(String denyPattern:pattern_to_deny_pattern.get(pa)){
                     denyPatternSet.add(denyPattern);
                 }
             }
 
             sb=new StringBuilder();
-            for(String candidatePattern:patternList){
-                if (!denyPatternSet.contains(candidatePattern)){
-                    if(!pattern_to_class.containsKey(candidatePattern))
-                        System.out.println("error "+candidatePattern+" not included in pattern_to_class");
-                    else
-                        for(String candidateClass:pattern_to_class.get(candidatePattern))
-                            if(sb.indexOf(candidateClass)==-1)
-                                sb.append(candidateClass+"|");
+
+            //需要包括且仅仅包括？ to-do 不然不能处理 		外资投资！！！！	投资*苹果*产品-苹果*产品
+            for(int i=0;i<phaseList.length;i++){
+                boolean flag1=false;
+                for(String s:denyPatternSet){
+                    if(s.contains(".*")){
+                        String[] al = s.split("\\.\\*");
+                        boolean flag=true;
+                        StringBuilder sBuilder=new StringBuilder();
+                        for(String e: al) {
+                            sBuilder.append(e+"|");
+                            Matcher pa=Pattern.compile(e).matcher(phaseList[i]);
+                            if (!pa.find()) {
+                                flag=false;
+                                break;
+                            }
+                        }
+                        if(flag)//包含
+                        {
+                            String temp=phaseList[i];
+                            if (temp.replaceAll(sBuilder.substring(0,sBuilder.length()-1),"").length()==0)//仅仅包含
+                             flag1 = true;
+                        }
+                    }else{
+                        Matcher pa=Pattern.compile(s).matcher(phaseList[i]);
+                        if(pa.matches()) {
+                           flag1=true;
+                        }
+                    }
                 }
+                if(!flag1)
+                    sb.append(classList[i]+"|");
             }
             if(sb.length()>0)
                 type=sb.substring(0,sb.length()-1);
+            //处理去除模式>>
         }
         return type ;// return type + "\t" + line; 修改
     }
 
-    //返回值是一个匹配上一个class时，匹配到的模式
+    //返回值是一个匹配上一个class时，匹配到某个的模式用的词组合+'\t'+匹配上的pattern
     private static String isKeyType(String regex, String line) {
         String[] regexs = regex.split("@");
         for(String s : regexs){
+            //System.out.println("debug s regexs"+s);
+            StringBuilder sb=new StringBuilder();
             if(s.contains(".*")){
                 String[] al = s.split("\\.\\*");
                 boolean flag = true;
                 for(String e: al) {
-					/*if(!line.contains(e)) {
-						flag = false;
-						break;
-					}*/
                     //System.out.println("debug"+e);
-                    if (!Pattern.compile(e).matcher(line).find()) {
+                    Matcher pa=Pattern.compile(e).matcher(line);
+                    if (!pa.find()) {
                         flag = false;
                         break;
                     }
+                    else
+                        sb.append(line.substring(pa.start(),pa.end()));
                 }
                 if(flag) {
                     //System.out.println("debug"+s);
-                    return s;
+                    sb.append("\t"+s);  //匹配到某个的模式用的词组合+'\t'+匹配上的pattern
+                    return sb.toString();
                 }
             }else{
-                if(Pattern.compile(s).matcher(line).find()) {
+                Matcher pa=Pattern.compile(s).matcher(line);
+                if(pa.find()) {
                     //System.out.println("debug"+s);
-                    return s;
+                    return line.substring(pa.start(),pa.end())+"\t"+s;
                 }
             }
         }
@@ -366,12 +448,12 @@ public class EXP {
     public static void main(String[] args) throws Exception
     {
         //just for test
-        Vector<String> vTemp=new Vector<>();vTemp.add("葡萄.*种植");
-        pattern_to_deny_pattern.put("种植.*投资",vTemp);
+        //Vector<String> vTemp=new Vector<>();vTemp.add("葡萄.*种植");
+        //pattern_to_deny_pattern.put("种植.*投资",vTemp);
         //just for test
 
-        treeCount=new myTreeKount("dat/问题类别模式.txt");
         loadPattern("dat/问题类别模式.txt");
+        treeCount=new myTreeKount("dat/问题类别模式.txt");
         processCluster("dat/prase.in","dat/prase_out.txt");
 
         File fileOut = new File("dat/贷款人所在行业.txt");
